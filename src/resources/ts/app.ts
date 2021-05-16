07a491b9b61ef341988d35f5793bca6aeed2fba3
shimotaroo <shimotaro.0327@gmail.com>
2021-05-16 16:04:26 +0900

    Vuetify追加

--- src/resources/ts/app.ts
+++ src/resources/ts/app.ts
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 // import router from './router'
 // import App from './App.vue'
+import Vuetify from "vuetify";
+
+Vue.use(Vuetify);
 
 new Vue({
     el: '#app',
     // router,
     // components: { App },
-    // template: '<App />'
-    template: '<h1>Hello world!!!</h1>'
+    // template: '<App />',
+    template: '<h1>Hello world!!!</h1>',
+    vuetify: new Vuetify()
 })

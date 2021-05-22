127ef274638a295ff49c0db4f0556615691be020
shimotaroo <shimotaro.0327@gmail.com>
2021-05-22 23:53:29 +0900

    Vuex導入

--- src/resources/ts/app.ts
+++ src/resources/ts/app.ts
@@ -1,16 +1,29 @@
+// Ajax通信で用いるAxiosライブラリの設定を読み込む
+import './bootstrap'
+
+// import Vue.js
 import Vue from 'vue'
+// import Vue ROuter
 import router from './router'
+
+// ipmpoert App.vue
 import App from './App.vue'
+
+// Vuetify（アイコン、フォント含む）
 import Vuetify from "vuetify"
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@mdi/font/css/materialdesignicons.css'
+
+// import store(Vuex)
+import store from "./store"
 
 Vue.use(Vuetify);
 
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App),
     vuetify: new Vuetify()
 })
---
+++ src/resources/ts/store/auth.ts
@@ -0,0 +1,16 @@
+const state = {}
+
+const getters = {}
+
+const mutations = {}
+
+const actions = {}
+
+// モジュールに分けたときにステートやミューテーションの名前が被ってもモジュール名で区別できる
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}

8de6f606ca746f7a4d9747731ff92c75b63eebb7
shimotaroo <shimotaro.0327@gmail.com>
2021-06-08 23:08:51 +0900

    コントローラー追加

---
+++ src/app/Http/Controllers/Article/StoreController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Article;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ArticleStoreRequest;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function store (ArticleStoreRequest $request)
+    {
+    }
+}

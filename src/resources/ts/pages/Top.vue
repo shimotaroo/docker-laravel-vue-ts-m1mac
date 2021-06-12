ea1775b99b1a1e453c68844b33099a45f8035d62
shimotaroo <shimotaro.0327@gmail.com>
2021-06-12 12:47:33 +0900

    「詳細を見る」ボタンを追加

--- src/resources/ts/pages/Top.vue
+++ src/resources/ts/pages/Top.vue
@@ -8,20 +8,28 @@
       >
         <v-card class="pa-2">
           <v-card-text>
-            <v-icon class="mr-1">mdi-account-edit</v-icon>{{ article.user.name }}
+            <v-icon class="mr-2 ml-5">mdi-account-edit</v-icon>{{ article.user.name }}
+            <v-divider class="mt-5"></v-divider>
             <v-row class="mt-5">
               <v-col cols="4" class="text-right">タイトル</v-col>
-              <v-col cols="6">{{ article.title }}</v-col>
+              <v-col cols="8">{{ article.title }}</v-col>
             </v-row>
             <v-row>
               <v-col cols="4" class="text-right">URL</v-col>
-              <v-col cols="6"><a :href="article.url">{{ article.url }}</a></v-col>
+              <v-col cols="8"><a :href="article.url">{{ article.url }}</a></v-col>
             </v-row>
             <v-row>
               <v-col cols="4" class="text-right">投稿日時</v-col>
-              <v-col cols="6">{{ article.created_at }}</v-col>
+              <v-col cols="8">{{ article.created_at }}</v-col>
             </v-row>
           </v-card-text>
+          <v-card-actions class="mb-5">
+            <v-btn class="text-center mx-auto blue py-2 px-4">
+              <router-link to="/login">
+                <v-icon class="mr-2">mdi-dots-horizontal-circle-outline</v-icon>詳細を見る
+              </router-link>
+            </v-btn>
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>

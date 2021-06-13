608611d3da66f5ffe09edc175b5596162e92846a
shimotaroo <shimotaro.0327@gmail.com>
2021-06-13 15:50:21 +0900

    一覧画面の記事データ表示部分を別コンポーネントに切り出し

---
+++ src/resources/ts/components/Article.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-col>
+    <v-card class="pa-2">
+      <v-card-text>
+        <v-icon class="mr-2 ml-5">mdi-account-edit</v-icon>{{ article.user.name }}
+        <v-divider class="mt-5"></v-divider>
+        <v-row class="mt-5">
+          <v-col cols="4" class="text-right">タイトル</v-col>
+          <v-col cols="8">{{ article.title }}</v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4" class="text-right">URL</v-col>
+          <v-col cols="8"><a :href="article.url">{{ article.url }}</a></v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4" class="text-right">投稿日時</v-col>
+          <v-col cols="8">{{ article.created_at }}</v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions class="mb-5">
+        <v-btn class="text-center mx-auto blue py-4 px-8">
+          <router-link :to="`/article/${article.id}`">
+            <v-icon class="mr-2">mdi-dots-horizontal-circle-outline</v-icon>詳細を見る
+          </router-link>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    article: {
+      type: Object,
+      required: false
+    }
+  }
+})
+</script>
--- src/resources/ts/pages/Top.vue
+++ src/resources/ts/pages/Top.vue
@@ -1,64 +1,47 @@
 <template>
   <v-container class="mt-15">
     <v-row>
-      <v-col
+      <Article
         v-for="(article, index) in articles"
         :key="index"
-        cols="6"
-      >
-        <v-card class="pa-2">
-          <v-card-text>
-            <v-icon class="mr-2 ml-5">mdi-account-edit</v-icon>{{ article.user.name }}
-            <v-divider class="mt-5"></v-divider>
-            <v-row class="mt-5">
-              <v-col cols="4" class="text-right">タイトル</v-col>
-              <v-col cols="8">{{ article.title }}</v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="4" class="text-right">URL</v-col>
-              <v-col cols="8"><a :href="article.url">{{ article.url }}</a></v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="4" class="text-right">投稿日時</v-col>
-              <v-col cols="8">{{ article.created_at }}</v-col>
-            </v-row>
-          </v-card-text>
-          <v-card-actions class="mb-5">
-            <v-btn class="text-center mx-auto blue py-2 px-4">
-              <router-link to="/login">
-                <v-icon class="mr-2">mdi-dots-horizontal-circle-outline</v-icon>詳細を見る
-              </router-link>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
+        :cols="6"
+        :article="article"
+      />
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import axios, { AxiosResponse } from "axios"
+// TypeScriptの場合この記述が必要
+import axios, { AxiosResponse } from 'axios'
+// コンポーネント
+import Article from '../components/Article.vue'
+// ステータスコード
+import { OK } from '../util'
 
 interface DataInterface {
   articles: object[],
-  count: number
 }
 
 export default Vue.extend({
-  data () {
+  components: {
+    Article
+  },
+  data (): DataInterface {
     return {
       articles: [],
-      count: 0
     }
   },
   methods: {
     async fetchArticles () {
       const response: AxiosResponse = await axios.get('api/articles')
+
+      if (response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+      }
+
       this.articles = response.data.data
-      this.count = this.articles.length
-      console.log(this.articles)
-      console.log(this.count)
     }
   },
   // $route を監視してページが切り替わったときに fetchArticles が実行される
